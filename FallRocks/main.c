16acc5ba35e4931ceda7589fc2127050965be9d8
Badast <118640295+Badastt@users.noreply.github.com>
2022-12-12 12:55:11 -0300

    Add files via upload

--- FallRocks/main.c
+++ FallRocks/main.c
@@ -1,34 +1,47 @@
 #include "raylib.h"
 #include <stdlib.h>
 
+#define MAX_FPS 60
+
+typedef struct {
+	Vector2 position;
+	Vector2 size;
+	Texture2D texture;
+	Rectangle frameRec;
+} Player;
+
 int main(void)
 {
 	int screenWidth = 1366;
 	int screenHeight = 768;
 	InitWindow(1366, 768, "FallRocks");
+	
+	Player Rill = {{screenWidth/2, screenHeight/2}, {32, 32}, LoadTexture("data/Rill3.png"), {0.0f, 0.0f, (float)Rill.texture.width/1, (float)Rill.texture.height/1}};
+
+	Camera2D camera = {{Rill.position.x+32, Rill.position.y+32}, (Vector2){screenWidth/2, screenHeight/2}, 0, 3};
 
 	bool Jump;
 	bool canJump;
 	int JumpTimer = 0;
-
+	unsigned int frameCounter = 0;
 	
-	Vector2 RillPosition = {screenWidth/2, screenHeight/2};
-	Vector2 RillSize = {60, 120};
-	//Rectangle frameRec = { 0.0f, 0.0f, (float)Rill.width/5, (float)Rill.height};
+	SetTargetFPS(MAX_FPS);
 	
-	SetTargetFPS(60);
-	
-	Rectangle floor = {0, screenHeight-85, screenWidth, 80};	
+	Rectangle floor = {0, screenHeight-85, screenWidth, 600};	
 
 	while (!WindowShouldClose())
 	{
-		bool onFloor = CheckCollisionPointRec ((Vector2){RillPosition.x+RillSize.x/2, RillPosition.y+RillSize.y}, floor);
+		frameCounter++;
+		frameCounter %= MAX_FPS;
+		camera.target = (Vector2){Rill.position.x+32, Rill.position.y+32};
+
+		bool onFloor = CheckCollisionPointRec ((Vector2){Rill.position.x+Rill.size.x/2, Rill.position.y+Rill.size.y}, floor);
 		
-		if (IsKeyDown(KEY_RIGHT)) RillPosition.x += 8.0f;
-		if (IsKeyDown(KEY_LEFT)) RillPosition.x -= 8.0f;
+		if (IsKeyDown(KEY_RIGHT)) Rill.position.x += 4.0f;
+		if (IsKeyDown(KEY_LEFT)) Rill.position.x -= 4.0f;
 		
-		if (RillPosition.y < 0 || RillPosition.y > screenHeight) RillPosition.y = screenHeight/2;
-		if (RillPosition.x < 0 || RillPosition.x > screenWidth) RillPosition.x =screenWidth/2;
+		if (Rill.position.y < 0 || Rill.position.y > screenHeight) Rill.position.y = screenHeight/2;
+		if (Rill.position.x < 0 || Rill.position.x > screenWidth) Rill.position.x =screenWidth/2;
 		
 		if (IsKeyDown(KEY_UP) && canJump) 
 			Jump = true;
@@ -38,20 +51,24 @@ int main(void)
 		if (Jump && canJump){
 			JumpTimer++; 
 			if (JumpTimer < 30)
-				RillPosition.y-=6;
+				Rill.position.y-=6;
 			else
 				canJump = 0;
 		} else if (!onFloor)
-			RillPosition.y+=6;
+			Rill.position.y+=6;
 
 		if (onFloor){Jump = false; JumpTimer = 0; canJump = true;}
 
 		BeginDrawing();
-		ClearBackground(WHITE);
+		ClearBackground(RAYWHITE);
+		BeginMode2D(camera);
 			DrawRectangleRec(floor, GREEN);
-			DrawRectangleV(RillPosition, RillSize, BLACK);
+			DrawTextureRec(Rill.texture, Rill.frameRec, Rill.position, WHITE);
+		EndMode2D();
 		EndDrawing();
 	}
+	
+	UnloadTexture(Rill.texture);
 
 	CloseWindow();
 
